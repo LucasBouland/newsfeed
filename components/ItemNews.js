a810341e9f17e069d173ea0c5c9ae3e69a4afb96
unknown <guillaume.arsac@hotmail.fr>
2019-12-06 16:19:38 +0100

    un bug

--- components/ItemNews.js
+++ components/ItemNews.js
@@ -1,84 +1,84 @@
 import React, { Component } from "react";
 import {
-  View,
-  Button,
-  StyleSheet,
-  Text,
-  Image,
-  SafeAreaView
+    View,
+    Button,
+    StyleSheet,
+    Text,
+    Image,
+    SafeAreaView
 } from "react-native";
 import PropTypes from "prop-types";
 import { SwipeRow } from "react-native-swipe-list-view";
 
 class ItemNews extends Component {
-  static propTypes = {
-    onDelete: PropTypes.func.isRequired,
-    news: PropTypes.any.isRequired
-  };
+    static propTypes = {
+        onDelete: PropTypes.func.isRequired,
+        news: PropTypes.any.isRequired
+    };
 
-  state = {};
+    state = {};
 
-  render() {
-    // titre, la source, la date de publication et une image.
-    //Les news lues sont stockées dans le storage via un reducer.
-    return (
-      <SwipeRow
-        leftOpenValue={75}
-        rightOpenValue={0}
-        key={this.props.news.title}
-      >
-        <View style={styles.standaloneRowBack}>
-          <Button
-            title="Suppr."
-            onPress={() => this.props.onDelete(this.props.news.title)}
-          ></Button>
-        </View>
-        <View style={styles.standaloneRowFront}>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "column",
-              justifyContent: "space-between"
-            }}
-          >
-            <Text>{"Titre: " + this.props.news.title}</Text>
-            <Text>{"Source:" + this.props.news.source.name}</Text>
-            <Image
-              style={{ width: 80, height: 80 }}
-              source={{ uri: this.props.news.urlToImage }}
-            />
-            <Text>{"Publié le:" + this.props.news.publishedAt}</Text>
-          </View>
-        </View>
-      </SwipeRow>
-    );
-  }
+    render() {
+        // titre, la source, la date de publication et une image.
+        //Les news lues sont stockées dans le storage via un reducer.
+        return (
+            <SwipeRow
+                leftOpenValue={75}
+                rightOpenValue={0}
+                key={this.props.news.title}
+            >
+                <View style={styles.standaloneRowBack}>
+                    <Button
+                        title="Lu."
+                        onPress={() => this.props.onDelete(this.props.news.title)}
+                    ></Button>
+                </View>
+                <View style={styles.standaloneRowFront}>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexDirection: "column",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                        <Text>{"Titre: " + this.props.news.title}</Text>
+                        <Text>{"Source:" + this.props.news.source.name}</Text>
+                        <Image
+                            style={{ width: 80, height: 80 }}
+                            source={{ uri: this.props.news.urlToImage }}
+                        />
+                        <Text>{"Publié le:" + this.props.news.publishedAt}</Text>
+                    </View>
+                </View>
+            </SwipeRow>
+        );
+    }
 }
 
 export default ItemNews;
 //  <DatePublished time={this.props.news.publishedAt}></DatePublished>
 const DatePublished = props => {
-  const dt = Date.parse(props.time);
-  const result = `${dt.now()}`;
-  return <Text>Publié le: {result}</Text>;
+    const dt = Date.parse(props.time);
+    const result = `${dt.now()}`;
+    return <Text>Publié le: {result}</Text>;
 };
 
 const styles = StyleSheet.create({
-  standaloneRowFront: {
-    alignItems: "center",
-    backgroundColor: "#CCC",
-    justifyContent: "center",
-    height: 180
-  },
-  standaloneRowBack: {
-    alignItems: "center",
-    backgroundColor: "#CCC",
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 15
-  },
-  backTextWhite: {
-    color: "#FFF"
-  }
+    standaloneRowFront: {
+        alignItems: "center",
+        backgroundColor: "#CCC",
+        justifyContent: "center",
+        height: 180
+    },
+    standaloneRowBack: {
+        alignItems: "center",
+        backgroundColor: "#CCC",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 15
+    },
+    backTextWhite: {
+        color: "#FFF"
+    }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,80 +7,101 @@ import { ActivityIndicator } from "react-native-paper";
 import _ from "lodash";
 
 export default class HomeScreen extends Component {
-  static navigationOptions = e => {
-    return {
-      title: "",
-      headerRight: (
-        <Icon
-          size={25}
-          name={"ios-add"}
-          onPress={() => {
-            e.navigation.push("Details");
-          }}
-        />
-      )
-    };
-  };
-
-  serviceNews = new NewsService();
-
-  state = { news: [], categories: [] };
-
-  async update() {
-    const categories = JSON.parse(await AsyncStorage.getItem("CATEGORIES"));
-    let allNews = [];
-    if (categories != null) {
-      for (const c of categories) {
-        cat = await this.serviceNews.getNewsByCategory(c);
-        allNews = [].concat(...cat.data.articles);
-      }
-      uniquesNews = _.uniqBy(allNews, "title");
-      this.setState({ news: uniquesNews });
-    }
-  }
-
-  componentDidMount() {
-    this.update();
-  }
-
-  delete = async newsName => {
-    const tab = this.state.articles.map(e => e.name);
-    tab.splice(
-      tab.findIndex(e => e === newsName),
-      1
-    );
-    await AsyncStorage.setItem("NEWS", JSON.stringify(tab));
-    this.update();
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        {this.state.news.length > 0 ? (
-          <FlatList
-            data={this.state.news}
-            renderItem={e => (
-              <>
-                <ItemNews
-                  key={e.item.title}
-                  news={e.item}
-                  onDelete={this.delete}
+    static navigationOptions = e => {
+        return {
+            title: "",
+            headerRight: (
+                <Icon
+                    size={25}
+                    name={"ios-add"}
+                    onPress={() => {
+                        e.navigation.push("Details");
+                    }}
                 />
-                <Button
-                  title="Details"
-                  onPress={() => {
-                    this.props.navigation.push("Details", {
-                      title: e.item.title
-                    });
-                  }}
-                ></Button>
-              </>
-            )}
-          />
-        ) : (
-          <ActivityIndicator />
-        )}
-      </View>
-    );
-  }
+            )
+        };
+    };
+
+    serviceNews = new NewsService();
+
+    state = { news: [], categories: [] };
+
+    async update() {
+        console.log('update start --------------------------------------------------------------------------------------------------------------------------------------------------');
+        const categories = JSON.parse(await AsyncStorage.getItem("CATEGORIES"));
+        let data = await AsyncStorage.getItem('NEWS');
+        AsyncStorage.clear();
+        console.log(data);
+        let allNews = [];
+        if (categories != null) {
+            for (const c of categories) {
+                cat = await this.serviceNews.getNewsByCategory(c);
+                allNews = [].concat(...cat.data.articles);
+            }
+            uniquesNews = _.uniqBy(allNews, "title");
+            console.log(data.length);
+            console.log(uniquesNews.length);
+            for (i = 0; i < uniquesNews.length; i++) {
+                for (j = 0; j < data.length; j++) {
+                    //console.log(i, j);
+                    if (uniquesNews[i].title == data[j]) {
+                        uniquesNews.splice(i, 1);
+                        i--;
+                        console.log(`deleted --------------- ${d.title} -----------------------------------------------------------------------------------`)
+                    }
+                }
+            }
+            this.setState({ news: uniquesNews });
+        }
+        console.log('update end ---------------------------------------------------------------------------------------------------------------------------');
+    }
+
+    componentDidMount() {
+        this.update();
+    }
+
+    delete = async newsName => {
+        console.log('delete start ----------------------------------------------------------------------------------------------------------------------------');
+        let data = await AsyncStorage.getItem('NEWS');
+        let tab = [];
+        if (data != null) {
+            tab = JSON.parse(data);
+        }
+        tab.push(newsName);
+        console.log(tab);
+        await AsyncStorage.setItem('NEWS', JSON.stringify(tab));
+        this.update();
+        console.log('delete end --------------------------------------------------------------------------------------------------------------------------');
+    };
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                {this.state.news.length > 0 ? (
+                    <FlatList
+                        data={this.state.news}
+                        renderItem={e => (
+                            <>
+                                <ItemNews
+                                    key={e.item.title}
+                                    news={e.item}
+                                    onDelete={this.delete}
+                                />
+                                <Button
+                                    title="Details"
+                                    onPress={() => {
+                                        this.props.navigation.push("Details", {
+                                            title: e.item.title
+                                        });
+                                    }}
+                                ></Button>
+                            </>
+                        )}
+                    />
+                ) : (
+                        <ActivityIndicator />
+                    )}
+            </View>
+        );
+    }
 }
