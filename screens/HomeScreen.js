0d070f86708e9ab6a0c0e98b3ce9c798f08fdbb8
LucasBouland <lucas.bouland@ynov.com>
2019-12-06 17:52:52 +0100

    fix rebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,33 +18,15 @@ export default class HomeScreen extends Component {
         />
       )
     };
+  };
 
-    serviceNews = new NewsService();
+  serviceNews = new NewsService();
 
-    state = { news: [], categories: [] };
+  state = { news: [], categories: [] };
 
-     update = async () => {
-        const categories = JSON.parse(await AsyncStorage.getItem("CATEGORIES"));
-        let data = await AsyncStorage.getItem("NEWS");
-        let allNews = [];
-        if (categories != null) {
-            for (const c of categories) {
-                cat = await this.serviceNews.getNewsByCategory(c);
-                allNews = [].concat(...cat.data.articles);
-            }
-            uniquesNews = _.uniqBy(allNews, "title");
-            if (data != null) {
-                uniquesNews = uniquesNews.filter(word => !data.includes(word.title));
-            }
-            this.setState({ news: uniquesNews });
-        }
-    }
-
-    componentDidMount() {
-        this.update();
-    }
   update = async () => {
     const categories = JSON.parse(await AsyncStorage.getItem("CATEGORIES"));
+    let data = await AsyncStorage.getItem("NEWS");
     let allNews = [];
     if (categories != null) {
       for (const c of categories) {
@@ -52,6 +34,9 @@ export default class HomeScreen extends Component {
         allNews = [].concat(...cat.data.articles);
       }
       uniquesNews = _.uniqBy(allNews, "title");
+      if (data != null) {
+        uniquesNews = uniquesNews.filter(word => !data.includes(word.title));
+      }
       this.setState({ news: uniquesNews });
     }
   };
@@ -61,50 +46,54 @@ export default class HomeScreen extends Component {
     this.update();
   }
 
-    delete = async newsName => {
-        console.log('delete start ----------------------------------------------------------------------------------------------------------------------------');
-        let data = await AsyncStorage.getItem('NEWS');
-        let tab = [];
-        if (data != null) {
-            tab = JSON.parse(data);
-        }
-        tab.push(newsName);
-        console.log(tab);
-        await AsyncStorage.setItem('NEWS', JSON.stringify(tab));
-        this.update();
-        console.log('delete end --------------------------------------------------------------------------------------------------------------------------');
-    };
-
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                {this.state.news.length > 0 ? (
-                    <FlatList
-                        data={this.state.news}
-                        renderItem={e => (
-                            <>
-                                <ItemNews
-                                    key={e.item.title}
-                                    news={e.item}
-                                    onDelete={this.delete}
-                                />
-                                <Button
-                                    title="Details"
-                                    onPress={() => {
-                                        console.log("ee", e.item);
-                                        this.props.navigation.push("Details", {
-                                            detailsArticle: e.item,
-                                        });
-                                    }}
-                                ></Button>
-                            </>
-                        )}
-                        keyExtractor={(item, index) => index.toString()}
-                    />
-                ) : (
-                        <ActivityIndicator />
-                    )}
-            </View>
-        );
+  delete = async newsName => {
+    console.log(
+      "delete start ----------------------------------------------------------------------------------------------------------------------------"
+    );
+    let data = await AsyncStorage.getItem("NEWS");
+    let tab = [];
+    if (data != null) {
+      tab = JSON.parse(data);
     }
+    tab.push(newsName);
+    console.log(tab);
+    await AsyncStorage.setItem("NEWS", JSON.stringify(tab));
+    this.update();
+    console.log(
+      "delete end --------------------------------------------------------------------------------------------------------------------------"
+    );
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        {this.state.news.length > 0 ? (
+          <FlatList
+            data={this.state.news}
+            renderItem={e => (
+              <>
+                <ItemNews
+                  key={e.item.title}
+                  news={e.item}
+                  onDelete={this.delete}
+                />
+                <Button
+                  title="Details"
+                  onPress={() => {
+                    console.log("ee", e.item);
+                    this.props.navigation.push("Details", {
+                      detailsArticle: e.item
+                    });
+                  }}
+                ></Button>
+              </>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        ) : (
+          <ActivityIndicator />
+        )}
+      </View>
+    );
+  }
 }
