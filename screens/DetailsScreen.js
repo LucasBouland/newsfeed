d20733ce2e2d9769e0fed4d8ae8fa4ffa30ec9bb
unknown <guillaume.arsac@hotmail.fr>
2019-12-06 14:42:57 +0100

    a

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -4,77 +4,78 @@ import NewsService from "../services/NewsService";
 import Loading from "../components/Loading";
 
 export default class DetailsScreen extends Component {
-  state = { data: null };
+    state = { data: null };
 
-  serv = new NewsService();
+    serv = new NewsService();
 
-  componentDidMount() {
-    this.serv
-      .getNewsByTitle(this.props.navigation.getParam("title", "NONE"))
-      .then(resp => {
-        this.setState({ data: resp.data });
-        console.log(this.state.data);
-        console.log(this.props.navigation.getParam("title"));
-      });
-  }
+    componentDidMount() {
+        this.serv
+            .getNewsByTitle(this.props.navigation.getParam("title", "NONE"))
+            .then(resp => {
+                this.setState({ data: resp.data });
+                console.log(resp, 'a');
+                console.log(this.state.data, 'a');
+                console.log(this.props.navigation.getParam("title"), 'a a');
+            });
+    }
 
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 2, marginTop: 10 }}>
-        {this.state.data ? (
-          <>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text>{this.state.data.articles[0].title}</Text>
-            </View>
-            <View
-              style={{
-                flex: 2,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text>image</Text>
-            </View>
-            <View
-              style={{
-                flex: 3,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text>{this.state.data.articles[0].publishedAt}</Text>
-            </View>
-            <View
-              style={{
-                flex: 4,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text>{this.state.data.articles[0].author}</Text>
-            </View>
-            <View
-              style={{
-                flex: 5,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text>{this.state.data.articles[0].content}</Text>
-            </View>
-          </>
-        ) : (
-          <Loading displayColor="orange"></Loading>
-        )}
-      </SafeAreaView>
-    );
-  }
+    render() {
+        return (
+            <SafeAreaView style={{ flex: 2, marginTop: 10 }}>
+                {this.state.data ? (
+                    <>
+                        <View
+                            style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Text>{this.state.data.articles[0].title}</Text>
+                        </View>
+                        <View
+                            style={{
+                                flex: 2,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Image source={this.state.data.articles[0].urlToImage} />
+                        </View>
+                        <View
+                            style={{
+                                flex: 3,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Text>{this.state.data.articles[0].publishedAt}</Text>
+                        </View>
+                        <View
+                            style={{
+                                flex: 4,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Text>{this.state.data.articles[0].author}</Text>
+                        </View>
+                        <View
+                            style={{
+                                flex: 5,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Text>{this.state.data.articles[0].content}</Text>
+                        </View>
+                    </>
+                ) : (
+                        <Loading displayColor="orange"></Loading>
+                    )}
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({});
