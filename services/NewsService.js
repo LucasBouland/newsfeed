e48245b8fc754300179a26916b99ad68202aa291
LucasBouland <lucas.bouland@ynov.com>
2019-12-06 11:20:57 +0100

    fix API params + URL for api

--- services/NewsService.js
+++ services/NewsService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 const key = 'a20b31ac48b04fa399e1b38500a0f4fc';
-const url = `https://newsapi.org/v2/everything?apiKey=${key}`;
+const url = `https://newsapi.org/v2/top-headlines?apiKey=${key}`;
 
 class NewsService {
-    getNewsByKeyword(keyword = 'bitcoin') {
-        console.log(`${url}&q=${keyword}`);
+    getNewsByKeyword(keyword = 'general') {
+        console.log(`${url}&category=${keyword}`);
         return axios.get(`${url}&q=${keyword}`);
     }
 }
